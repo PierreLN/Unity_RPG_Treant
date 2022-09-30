fd9cce96f4d29350f4505cac524a874d090803bb
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2022-09-30 16:12:54 -0400

    2

--- Mini_RPG_exercice-main/Mini_RPG/Assets/Tiny RPG Forest/Chasseur.cs
+++ Mini_RPG_exercice-main/Mini_RPG/Assets/Tiny RPG Forest/Chasseur.cs
@@ -10,7 +10,9 @@ public class Chasseur : MonoBehaviour
     public GameObject cible; // Pas besoin de définir exactement c'est quoi la cible
     public GameObject explosion;
     public GameObject joueur;
-    public float speed = 3.0f;
+    public float speed = 1.0f;
+    private bool estActif = false;
+    public float distanceVue = 100.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,26 +26,42 @@ public class Chasseur : MonoBehaviour
         //Choisir le mouvement
         Vector3 direction = cible.transform.position - transform.position;
         mouvement = direction.normalized;
-
         // Regarder dans une direction, c'est souvent réglé par une soustraction de vecteur
 
-        // Animation
-        anim.SetFloat("Horizontal", mouvement.x);
-        anim.SetFloat("Vertical", mouvement.y);
-        anim.SetFloat("Speed", mouvement.sqrMagnitude);
+        estActif = Physics.Raycast(transform.position, mouvement * distanceVue);
 
-        // walk-front_joueur = x : 0 y : -1
-        // walk-back_joueur = x : 0 y : 1
-        // walk-left = x : -1 y : 0
-        // walk-right = x : 1 y : 0
+        if (estActif)
+        {
 
 
+
+            // Animation
+            anim.SetFloat("Horizontal", mouvement.x);
+            anim.SetFloat("Vertical", mouvement.y);
+            anim.SetFloat("Speed", mouvement.sqrMagnitude);
+
+            // walk-front_joueur = x : 0 y : -1
+            // walk-back_joueur = x : 0 y : 1
+            // walk-left = x : -1 y : 0
+            // walk-right = x : 1 y : 0
+
+        }
+        else
+        {
+
+        }
     }
 
     private void FixedUpdate()
     {
-        transform.position = transform.position + mouvement * speed * Time.fixedDeltaTime;
+        if (estActif)
+        {
+            transform.position = transform.position + mouvement * speed * Time.fixedDeltaTime;
+        }
+        else
+        {
 
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
